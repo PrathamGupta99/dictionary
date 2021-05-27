127d54d8c14ac2703957c051b5afb9da9ecd83ad
PrathamGupta99 <65537882+PrathamGupta99@users.noreply.github.com>
2021-05-27 16:34:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -27,22 +27,22 @@ function searchButtonHandler() {
 
     xhr.open('GET', `https://api.dictionaryapi.dev/api/v2/entries/${lang}/${word}`, true)
     // xhr.open('GET', `https://api.dictionaryapi.dev/api/v2/entries/hi/guru`, true)
-   
+
     let tableBody = document.getElementById('tbody')
     xhr.onload = function () {
         console.log("Fetching Starts")
         tableBody.innerHTML = ''
         str = `<thead>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Word</th>
-                        <th scope="col">Part of Speech</th>
-                        <th scope="col">Definition</th>
-                        <th scope="col" id="show">Examples</th>
+                        <th style="text-align: center; padding: 12px; font-size: 22px; scope="col" font-family: 'PT Serif';">#</th>
+                        <th style="text-align: center; padding: 12px; font-size: 22px; scope="col" font-family: 'PT Serif';">Word</th>
+                        <th style="text-align: center; padding: 12px; font-size: 22px; scope="col" font-family: 'PT Serif';">Part of Speech</th>
+                        <th style="text-align: center; padding: 12px; font-size: 22px; scope="col" font-family: 'PT Serif';">Definition</th>
+                        <th style="text-align: center; padding: 12px; font-size: 22px; scope="col" font-family: 'PT Serif';" id="show">Examples</th>
                     </tr>
                 </thead>`
         tableBody.innerHTML += str
-        str=''
+        str = ''
         if (this.status === 200 && english.checked) {
             let show = document.getElementById("show")
             show.classList.remove(`none`)
@@ -60,17 +60,17 @@ function searchButtonHandler() {
                     for (let j = 0; j < Array.from(data[k].meanings[i].definitions).length; j++) {
                         console.log(data[k].meanings[i].definitions[j].definition)
                         console.log(data[k].meanings[i].definitions[j].example)
-                        if(data[k].meanings[i].definitions[j].example === undefined) {
+                        if (data[k].meanings[i].definitions[j].example === undefined) {
                             data[k].meanings[i].definitions[j].example = ''
-                        } 
+                        }
                         str = `<tr>
-                                <th scope="row">${c+1}</th>
-                                <td style="text-align:justify;" class="justify">${data[k].word}</td>
-                                <td style="text-align:justify;" class="justify">${data[k].meanings[i].partOfSpeech}</td>
-                                <td style="text-align:justify;" class="justify">${data[k].meanings[i].definitions[j].definition}</td>
-                                <td style="text-align:justify;" class="justify">${data[k].meanings[i].definitions[j].example}</td>
+                                <th scope="row">${c + 1}</th>
+                                <td style="text-align: justify; font-size: 1rem; padding: 13px; font-family: ubuntu;" class="justify">${data[k].word}</td>
+                                <td style="text-align: justify; font-size: 1rem; padding: 13px; font-family: ubuntu;" class="justify">${data[k].meanings[i].partOfSpeech}</td>
+                                <td style="text-align: justify; font-size: 1rem; padding: 13px; font-family: ubuntu;" class="justify">${data[k].meanings[i].definitions[j].definition}</td>
+                                <td style="text-align: justify; font-size: 1rem; padding: 13px; font-family: ubuntu;" class="justify">${data[k].meanings[i].definitions[j].example}</td>
                             </tr>`
-                            c = c+1
+                        c = c + 1
                         tableBody.innerHTML += str
                         search.value = ''
                     }
@@ -96,16 +96,16 @@ function searchButtonHandler() {
                     for (let j = 0; j < Array.from(data[k].meanings[i].definitions).length; j++) {
                         console.log(data[k].meanings[i].definitions[j].definition)
                         console.log(data[k].meanings[i].definitions[j].example)
-                        if(data[k].meanings[i].definitions[j].example === undefined) {
+                        if (data[k].meanings[i].definitions[j].example === undefined) {
                             data[k].meanings[i].definitions[j].example = ''
-                        } 
+                        }
                         str = `<tr>
-                                <th scope="row">${c+1}</th>
+                                <th scope="row">${c + 1}</th>
                                 <td style="text-align:justify;" class="justify">${data[k].word}</td>
                                 <td style="text-align:justify;" class="justify">${data[k].meanings[i].partOfSpeech}</td>
                                 <td style="text-align:justify;" class="justify">${data[k].meanings[i].definitions[j].definition}</td>
                             </tr>`
-                            c = c+1
+                        c = c + 1
                         tableBody.innerHTML += str
                         search.value = ''
                     }
