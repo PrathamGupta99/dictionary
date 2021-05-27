9da0af6edca47b5380745225199a9b2624d87d1d
PrathamGupta99 <65537882+PrathamGupta99@users.noreply.github.com>
2021-05-27 22:30:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function searchButtonHandler() {
     let word = search.value
     console.log(word)
 
-    xhr.open('GET', `https://newsapi.org/v2/top-headlines?country=in&pageSize=100&sortBy=publishedAt&apiKey=b7ae0b7347264f3a8815a10873840290`, true)
+    xhr.open('GET', `https://saurav.tech/NewsAPI/top-headlines/category/health/in.json`, true)
     // xhr.open('GET', `https://api.dictionaryapi.dev/api/v2/entries/hi/guru`, true)
 
     let tableBody = document.getElementById('tbody')
